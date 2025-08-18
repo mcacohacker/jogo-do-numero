e9674c4d4cb0b171e6965316ea2cc5184a3631ef
mcacohacker <yasmim.isabeli.costa@escola.pr.gov.br>
2025-08-18 10:28:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-
+let numeroAleatorio = Math.floor(Math.random() *100) + 1;
 const palpites = document.querySelector('palpites'):
 const ultimoResultado = document.querySelector('ultimoResultado');
-conss
+const baixoOuAlto = document.querySelector('baixoOuAlto');
+const envioPalpite = document.querySelector('envioPalpite');
+const campoPalpite = document.querySelector('campoPalpite')
+let constagemPalpites = 1;
+let botaoReiniciar;
+
+function verificarPalpites() {
+  const palpiteUsuario = Number(campoPalpite.value);
+  if (contagemPapites === 1) {
+    palpites.textContent = "Palpites anteriores: ";
+  }
+
+palpites.textContent += palpiteUsuario + " ";
+
+if (palpiteUsuario === numeroAleatorio) {
