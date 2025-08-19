e18392213a6dfe3d9a84a4d2777d28407911db9a
mcacohacker <yasmim.isabeli.costa@escola.pr.gov.br>
2025-08-19 11:18:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,46 @@ function verificarPalpites() {
 palpites.textContent += palpiteUsuario + " ";
 
 if (palpiteUsuario === numeroAleatorio) {
+  ultimoResultado.textContent = "Parabéns! Você acertou!";
+  ultimoResdultado.style.backgroundColor = "green";
+  baixoOuAlto.textContent = "";
+  finalizarJogo();
+} else if (contagemPalpites === 10) {
+  ultimoResultado.textcontent = "FIM DE JOGO!!";
+  baixoOuAlto.textcontent = "";
+  finalizarJogo();
+} else {
+  ultimoResultado.textContent = "Errado";
+  ultimoResultado.style.backgroundColor = "red";
+  if (palpiteUsuario < numeroAleatorio) {
+    baixoOuAlto.textContent = "o ultimo palpite foi muito baixo";
+  } else if (palpiteUsuario > numeroAleatorio) {
+    baixoOuAlto.textContent = "o ultimo palpite foi muito baixo";
+  }
+}
+
+  contagemPalpites++;
+  campoPalpite.value = "";
+  campoPalpite.focus();
+}
+
+envioPalpite.addEventListener('click', verificarPalpite);
+
+function finalzarJogo() {
+  campoPalpite.disableb = true;
+  envioPalpite.disableb = true;
+  botaoReiniciar = document.CreateElement('button');
+  doccument.body.appendChild(botaoReiniciar);
+  botaoReiniciar.textContent = 'Reiniciar Jogo'
+  botaoReiniciar.classList.add('botaoReiniciar');
+  document.body.appendChild(botaoReiniciar);
+  botaoReiniciar.addEventListener('click', reiniciar jogo);
+}
+
+function reinicarJogo() {
+  contagemPalpites = 1;
+  const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultados p');
+  for (const paragrafoReiniciar of paragrafosReiniciar) {
+    paragrafoReiniciar.textcontent = "";
+  }
+}
