d48f7afc1c10708096124061608b400898c3dcc1
mcacohacker <yasmim.isabeli.costa@escola.pr.gov.br>
2025-08-20 07:39:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,68 +1,72 @@
-let numeroAleatorio = Math.floor(Math.random() *100) + 1;
-const palpites = document.querySelector('palpites'):
-const ultimoResultado = document.querySelector('ultimoResultado');
-const baixoOuAlto = document.querySelector('baixoOuAlto');
-const envioPalpite = document.querySelector('envioPalpite');
-const campoPalpite = document.querySelector('campoPalpite')
-let constagemPalpites = 1;
+let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+
+const palpites = document.querySelector('.palpites');
+const ultimoResultado = document.querySelector('.ultimoResultado');
+const baixoOuAlto = document.querySelector('.baixoOuAlto');
+const envioPalpite = document.querySelector('.envioPalpite');
+const campoPalpite = document.querySelector('.campoPalpite');
+
+let contagemPalpites = 1;
 let botaoReiniciar;
 
 function verificarPalpites() {
-  const palpiteUsuario = Number(campoPalpite.value);
-  if (contagemPapites === 1) {
-    palpites.textContent = "Palpites anteriores: ";
-  }
+    const palpiteUsuario = Number(campoPalpite.value);
+    if (contagemPalpites === 1) {
+        palpites.textContent = 'Palpites anteriores: ';
+    }
 
-palpites.textContent += palpiteUsuario + " ";
+    palpites.textContent += palpiteUsuario + ' ';
 
-if (palpiteUsuario === numeroAleatorio) {
-  ultimoResultado.textContent = "Parabéns! Você acertou!";
-  ultimoResdultado.style.backgroundColor = "green";
-  baixoOuAlto.textContent = "";
-  finalizarJogo();
-} else if (contagemPalpites === 10) {
-  ultimoResultado.textcontent = "FIM DE JOGO!!";
-  baixoOuAlto.textcontent = "";
-  finalizarJogo();
-} else {
-  ultimoResultado.textContent = "Errado";
-  ultimoResultado.style.backgroundColor = "red";
-  if (palpiteUsuario < numeroAleatorio) {
-    baixoOuAlto.textContent = "o ultimo palpite foi muito baixo";
-  } else if (palpiteUsuario > numeroAleatorio) {
-    baixoOuAlto.textContent = "o ultimo palpite foi muito baixo";
-  }
+    if (palpiteUsuario === numeroAleatorio) {
+        ultimoResultado.textContent = 'Parabéns! Você acertou!';
+        ultimoResultado.style.backgroundColor = 'green';
+        baixoOuAlto.textContent = '';
+        finalizarJogo();
+    } else if (contagemPalpites === 10) {
+        ultimoResultado.textContent = 'FIM DE JOGO!!';
+        baixoOuAlto.textContent = '';
+        finalizarJogo();
+    } else {
+        ultimoResultado.textContent = 'Errado!';
+        ultimoResultado.style.backgroundColor = 'red';
+        if (palpiteUsuario < numeroAleatorio) {
+            baixoOuAlto.textContent = 'O último palpite foi muito BAIXO!';
+        } else if (palpiteUsuario > numeroAleatorio) {
+            baixoOuAlto.textContent = 'O último palpite foi muito ALTO!';
+        }
+    }
+
+    contagemPalpites++;
+    campoPalpite.value = '';
+    campoPalpite.focus();
 }
 
-  contagemPalpites++;
-  campoPalpite.value = "";
-  campoPalpite.focus();
+envioPalpite.addEventListener('click', verificarPalpites);
+
+function finalizarJogo() {
+    campoPalpite.disabled = true;
+    envioPalpite.disabled = true;
+    botaoReiniciar = document.createElement('button');
+    botaoReiniciar.textContent = 'Reiniciar Jogo';
+    botaoReiniciar.classList.add('botaoReiniciar'); 
+    document.body.appendChild(botaoReiniciar);
+    botaoReiniciar.addEventListener('click', reiniciarJogo);
 }
 
-envioPalpite.addEventListener('click', verificarPalpite);
+function reiniciarJogo() {
+    contagemPalpites = 1;
+    const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultado p');
+    for (const paragrafo of paragrafosReiniciar) {
+        paragrafo.textContent = '';
+    }
 
-function finalzarJogo() {
-  campoPalpite.disableb = true;
-  envioPalpite.disableb = true;
-  botaoReiniciar = document.CreateElement('button');
-  doccument.body.appendChild(botaoReiniciar);
-  botaoReiniciar.textContent = 'Reiniciar Jogo'
-  botaoReiniciar.classList.add('botaoReiniciar');
-  botaoReiniciar.addEventListener('click', reiniciar jogo);
-}
-
-function reinicarJogo() {
-  contagemPalpites = 1;
-  const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultados p');
-  for (const paragrafoReiniciar of paragrafosReiniciar) {
-    paragrafoReiniciar.textcontent = "";
-  }
-
-  botaoReiniciar.parentNode.removeChild(botaoReiniciar);
-  campoPalpite.disabled = false;
-  envioPalpite.disabled = false;
-  campoPalpite.value = "";
-  campoPalpite.focus();
-  ultimoResultado.style.backgroundColor = 'white';
-  numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    botaoReiniciar.parentNode.removeChild(botaoReiniciar);
+    campoPalpite.disabled = false;
+    envioPalpite.disabled = false;
+    campoPalpite.value = '';
+    campoPalpite.focus();
+    ultimoResultado.style.backgroundColor = 'transparent'; 
+    ultimoResultado.textContent = '';
+    palpites.textContent = '';
+    numeroAleatorio = Math.floor(Math.random() * 100) + 1;
 }
